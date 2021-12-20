69fa35db291aa2962de47b9dd43a0a6fd62f3de5
OfirEzra <ofir.ezra@agritask.com>
2021-12-20 22:32:05 +0200

    sticky nav

--- script.js
+++ script.js
@@ -3,3 +3,24 @@ mobileMenuBtn.addEventListener("click", function () {
   const headerElement = document.querySelector(".nav-block");
   headerElement.classList.toggle("nav-open");
 });
+
+const heroSection = document.querySelector(".hero-section");
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+    if (ent.isIntersecting === false) {
+      heroSection.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      heroSection.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(heroSection);
